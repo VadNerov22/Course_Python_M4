115df5c6c50020bd7269c5f7b48b1894ec33a858
VadNerov22 <vadoleg@mail.ru>
2023-10-08 13:20:47 +0300

    Выполненная ЛР 4 Модуля 4 и Зачёт

--- Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -10,7 +10,27 @@ def car_paths(n: int, m: int) -> List[List[int]]:
 
     :return: Новую таблицу с посчитанным количеством маршрутов в каждую клетку
     """
-    ...  # TODO решить задачу с помощью динамического программирования
+    table_path = [[0] * m for _ in range(n)]
+
+    table_path[0][0] = 1
+
+    # Столбец
+    for row_ind in range(n - 1):
+        table_path[row_ind + 1][0] += table_path[row_ind][0]
+
+    # Строка
+    for col_ind in range(m - 1):
+        table_path[0][col_ind + 1] += table_path[0][col_ind]
+
+    # Диагональ
+    for row_ind in range(n - 1):
+        for col_ind in range(m - 1):
+            table_path[row_ind + 1][col_ind + 1] += table_path[row_ind][col_ind]
+
+    for i in range(1, n):
+        for j in range(1, m):
+            table_path[i][j] = table_path[i - 1][j] + table_path[i][j - 1] + table_path[i - 1][j - 1]
+    return table_path
 
 
 if __name__ == '__main__':
